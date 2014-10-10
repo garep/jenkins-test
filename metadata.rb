e0917d519ac0334728835b9687982bcea716e316
unknown <phelpsga@PHELPSGA1.americas.hpqcorp.net>
2014-10-09 20:34:51 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'jenkins-test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures jenkins-test'
+long_description 'Installs/Configures jenkins-test'
+
+version          '0.1.0'
+
+
